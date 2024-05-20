3b69da158f67a7d5c36f4a974351a61b63953ac6
ProgrammerShahJalal <you@example.com>
2024-05-21 00:31:44 +0600

    basic setup

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
 import app from "./app";
+import config from "./config";
 
 async function main() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://next-level:shAumxYQfUwubQZH@cluster0.tllgu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(config.db_url as string);
 
-    const port = 5000;
-
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
